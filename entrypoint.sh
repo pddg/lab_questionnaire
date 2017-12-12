8ea157132d044a228791990668c5b44c10c42132
rascal <email@erscl.org>
2017-12-13 01:20:05 +0900

    [fix] database書き込めないやつ（-Rわすれてた）

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ export LC_ALL="en_US.UTF-8"
 cd /home
 git pull
 python3 src/lab_questionnaire/manage.py migrate
-chown www-data /home/src/lab_questionnaire/database/db.sqlite3
+chown -R www-data /home
 uwsgi --ini uwsgi.ini
