6885cb858decf5a6f39534a9b5d48b88e5aabb90
rascal <email@erscl.org>
2017-12-13 01:46:27 +0900

    [update] 集計部分の表示整えたり

--- src/lab_questionnaire/questionnaire/models.py
+++ src/lab_questionnaire/questionnaire/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 class StudyOffice(models.Model):
+    number = models.IntegerField(unique=False, default=0, verbose_name=u"番号")
     name = models.CharField(max_length=128, unique=True)
+    supervisor = models.CharField(max_length=128, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
     capacity = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
 
     class Meta:
+        ordering = ['number']
         verbose_name = u"研究室"
